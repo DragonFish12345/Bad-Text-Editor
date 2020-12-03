2d7215906546100ecbbc877172bda2a51ce0f1cd
DragonFish12345 <67212276+DragonFish12345@users.noreply.github.com>
2020-12-02 19:21:23 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ root = tk.Tk()
 root.title('1 Text Editor')
 root.geometry('500x500')
 
-welcome_label = tk.Label(root, text='1 Text Editor\nThe Worst Text Editor in World of Programming', bg='black')
+welcome_label = tk.Label(root, text='1 Text Editor\nThe Worst Text Editor in World of Programming', bg='sky blue')
 welcome_label.pack()
 
 openchoice = tk.Button(root, text='Open File')
 deletechoice = tk.Button(root, text='Delete a file')
+newchoice = tk.Button(root, text='New File')
+
+newchoice.pack()
+openchoice.pack()
+deletechoice.pack()
 
 
 root.mainloop()
